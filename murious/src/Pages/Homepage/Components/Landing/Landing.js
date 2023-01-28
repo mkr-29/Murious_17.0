33a04873e8ebc2b88fd152ec767c8d779cd69270
Mayank Kumar <mayank.k9802@gmail.com>
2023-01-28 23:57:37 +0530

    C3

--- murious/src/Pages/Homepage/Components/Landing/Landing.js
+++ murious/src/Pages/Homepage/Components/Landing/Landing.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import './Landing.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Landing.css";
+import Time from "./Time";
 
 export default function Landing() {
   return (
-    <div className='murious_landing'>
-
+    <div>
+      <div className="murious_landing">
+        <div className="murious_date">
+          24<sup>th</sup> - 26<sup>th</sup> February
+        </div>
+        <div className="murious_event_button">
+          <Link to="/" className="event_button_landing">
+            View Events
+          </Link>
+        </div>
+      </div>
+      <Time/>
     </div>
-  )
+  );
 }
